94d5d953c29410fc39e7f64092b31a8e3e898a5f
doffa-D <hossam.dagdag@gmail.com>
2023-03-09 16:39:48 +0100

    hide libft

--- commande.c
+++ commande.c
@@ -6,30 +6,30 @@
 /*   By: hdagdagu <hdagdagu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:19:24 by nouakhro          #+#    #+#             */
-/*   Updated: 2023/03/09 16:36:04 by hdagdagu         ###   ########.fr       */
+/*   Updated: 2023/03/09 16:39:20 by hdagdagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void    exicut_echo(t_all *my_struct)
-// {
-//     int len;
-//     len = 0;
-//     while(my_struct->cmd[len])
-//         len++;
-//     printf("%d\n",len); 
+void    exicut_echo(t_all *my_struct)
+{
+    int len;
+    len = 0;
+    while(my_struct->cmd[len])
+        len++;
+    printf("%d\n",len); 
 
 
 
 
     
-//     // int i;
-//     // int j;
-//     // i = 0;
-//     // j = 1;
-//     // while(j <)
-// }
+    // int i;
+    // int j;
+    // i = 0;
+    // j = 1;
+    // while(j <)
+}
 
 void exicut_commande(t_all my_struct, int i)
 {

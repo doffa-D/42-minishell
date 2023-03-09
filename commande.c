ea55aec1e175c97da7d6dc1a8028784cf05d14c0
Escanour-n <nourdineoukhroun@gmail.com>
2023-03-09 16:53:46 +0100

    push

--- commande.c
+++ commande.c
@@ -6,19 +6,19 @@
 /*   By: nouakhro <nouakhro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:19:24 by nouakhro          #+#    #+#             */
-/*   Updated: 2023/03/09 16:49:43 by nouakhro         ###   ########.fr       */
+/*   Updated: 2023/03/09 16:52:21 by nouakhro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    exicut_echo(t_all *my_struct)
-{
-    int len;
-    len = 0;
-    while(my_struct->cmd[len])
-        len++;
-    printf("%d\n",len);
+// void    exicut_echo(t_all *my_struct)
+// {
+//     int len;
+//     len = 0;
+//     while(my_struct->cmd[len])
+//         len++;
+//     printf("%d\n",len);
 
 
 
@@ -29,19 +29,19 @@ void    exicut_echo(t_all *my_struct)
 
 
     
-}
+// }
 
-void exicut_commande(t_all my_struct, int i)
-{
-    if(!ft_strncmp(my_struct.my_command[0], "cd", ft_strlen("cd")))
-    {
-		exit(1);
-    }
-    else if(!ft_strncmp(my_struct.my_command[0], "echo", ft_strlen("echo")))
-        exicut_echo(&my_struct);
-	else if(!ft_strncmp(my_struct.cmd, my_struct.cmd, ft_strlen(my_struct.cmd)))
-    {
-        check_rediractions(my_struct);
-        execve(my_struct.my_path[i], my_struct.my_command, NULL);
-    }
-}
+// void exicut_commande(t_all my_struct, int i)
+// {
+//     if(!ft_strncmp(my_struct.my_command[0], "cd", ft_strlen("cd")))
+//     {
+// 		exit(1);
+//     }
+//     else if(!ft_strncmp(my_struct.my_command[0], "echo", ft_strlen("echo")))
+//         exicut_echo(&my_struct);
+// 	else if(!ft_strncmp(my_struct.cmd, my_struct.cmd, ft_strlen(my_struct.cmd)))
+//     {
+//         check_rediractions(my_struct);
+//         execve(my_struct.my_path[i], my_struct.my_command, NULL);
+//     }
+// }

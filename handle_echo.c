8c1c5165c07ffb3a35dac89daf66b140ae491508
doffa-D <hossam.dagdag@gmail.com>
2023-03-10 18:03:59 +0100

    handle echo

---
+++ handle_echo.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_echo.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdagdagu <hdagdagu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 18:00:54 by hdagdagu          #+#    #+#             */
+/*   Updated: 2023/03/10 18:01:25 by hdagdagu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	check(t_all *my_struct, int i, int j, char *buff)
+{
+	if (my_struct->my_command[j][i - 1] == 34
+		&& my_struct->my_command[j][ft_strlen(my_struct->my_command[j])
+		- 1] == 34)
+		printf("%s", getenv(buff));
+	else if (my_struct->my_command[j][i - 1] == 39
+			&& my_struct->my_command[j][ft_strlen(my_struct->my_command[j])
+			- 1] == 39)
+		printf("$%s", buff);
+	else if (!my_struct->my_command[j][i - 1]
+			&& !my_struct->my_command[j][ft_strlen(my_struct->my_command[j])])
+		if (getenv(buff) != NULL)
+			printf("%s", getenv(buff));
+}
+
+void	handle_dollar(t_all *my_struct, int i, int j)
+{
+	int		x;
+	int		z;
+	char	*buff;
+
+	x = i + 1;
+	z = 0;
+	buff = malloc(ft_strlen(my_struct->my_command[j]));
+	if (!buff)
+		return ;
+	while (my_struct->my_command[j][x])
+	{
+		if (my_struct->my_command[j][x] != 34
+			&& my_struct->my_command[j][x] != '$'
+			&& my_struct->my_command[j][x] != 39
+			&& my_struct->my_command[j][x] != ' ')
+			buff[z++] = my_struct->my_command[j][x];
+		x++;
+	}
+	check(my_struct, i, j, buff);
+	free(buff);
+	i++;
+}
+
+void	check_dollar(t_all *my_struct, int i, int j)
+{
+	while ((size_t)i < ft_strlen(my_struct->my_command[j]))
+	{
+		if (my_struct->my_command[j][i] == 34)
+			i++;
+		else if (my_struct->my_command[j][i] == '$')
+		{
+			handle_dollar(my_struct, i, j);
+			break ;
+		}
+		else
+		{
+			if (my_struct->my_command[j][i] != 39)
+				printf("%c", my_struct->my_command[j][i]);
+			i++;
+		}
+	}
+}
+
+void	exicut_echo(t_all *my_struct)
+{
+	int	len;
+	int	i;
+	int	j;
+
+	len = 0;
+	while (my_struct->my_command[len])
+		len++;
+	my_struct->command_len = len;
+	i = 0;
+	j = 1;
+	while (my_struct->my_command[j])
+	{
+		i = 0;
+		check_dollar(my_struct, i, j);
+		printf(" ");
+		j++;
+	}
+	printf("\n");
+}
+
+int	quote_check(t_all *my_struct)
+{
+	int	i;
+	int	j;
+	int	count_1;
+	int	count;
+
+	i = -1;
+	count_1 = 0;
+	count = 0;
+	while (my_struct->my_command[++i])
+	{
+		j = -1;
+		while (my_struct->my_command[i][++j])
+		{
+			if (my_struct->my_command[i][j] == 34)
+				count_1++;
+			else if (my_struct->my_command[i][j] == 39)
+				count++;
+		}
+	}
+	if (count_1 % 2 == 1 && count_1 > 0)
+		return (0);
+	if (count % 2 == 1 && count > 0)
+		return (0);
+	return (1);
+}
--- minishell.h
+++ minishell.h
@@ -3,42 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nouakhro <nouakhro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hdagdagu <hdagdagu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:20:41 by nouakhro          #+#    #+#             */
-/*   Updated: 2023/03/09 16:08:06 by nouakhro         ###   ########.fr       */
+/*   Updated: 2023/03/10 18:03:42 by hdagdagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef MINISHELL_H
-#define MINISHELL_H
-#include "libft/libft.h"
-#include <stdio.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
+# define MINISHELL_H
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <limits.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
 
 typedef struct s_all
 {
+	size_t		linght_path;
+	char		**my_path;
+	char		**my_all_path;
+	char		*my_curent_path;
+	char		**my_command;
+	char		*read_all;
+	const char	*cmd;
+	int			command_len;
+}				t_all;
 
-	size_t  linght_path;
-	char **my_path;
-	char **my_all_path;
-	char *my_curent_path;
-	char **my_command;
-	char *read_all;
-	const char *cmd;
-	int command_len;
-}               t_all;
-
-void exicut_commande(t_all my_struct, int i);
-void check_rediractions(t_all my_struct);
-void get_the_path(t_all my_struct);
-void exit_the_program(t_all my_struct);
-void free_all(t_all my_struct);
+void			exicut_commande(t_all my_struct, int i);
+void			check_rediractions(t_all my_struct);
+void			get_the_path(t_all my_struct);
+void			exit_the_program(t_all my_struct);
+void			free_all(t_all my_struct);
+void			exicut_echo(t_all *my_struct);
+int				quote_check(t_all *my_struct);
 
 #endif
-

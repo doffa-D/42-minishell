52a6f4c10a235f032928df1ee8cb8d3edd17dbf0
Nourdine Ouakhroun <nouakhro@c1r3p5.1337.ma>
2023-05-04 15:09:23 +0100

    99.61%

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nouakhro <nouakhro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 15:07:52 by nouakhro          #+#    #+#             */
-/*   Updated: 2023/05/04 13:58:38 by nouakhro         ###   ########.fr       */
+/*   Updated: 2023/05/04 15:07:08 by nouakhro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,12 +111,33 @@ int cd_commade(t_all *my_struct, int c_of_s)
 }
 
 
+
 int if_builtins_in_parent(t_all *my_struct, int c_of_s)
 {
 	if(my_struct->each_cmd[c_of_s].cmd[0] && ft_strlen(my_struct->each_cmd[c_of_s].cmd[0]) \
 	 &&!ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "cd", ft_strlen(my_struct->each_cmd[c_of_s].cmd[0])))
 		return (1);
 	if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "export", ft_strlen("export")+1))
+		return (1);
+	else if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "env", ft_strlen("env")+1))
+		return (1);
+	else if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "unset", ft_strlen("unset")+1))
+		return (1);
+	else if (my_struct->each_cmd[c_of_s].cmd && my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "exit", ft_strlen("exit")))
+		return (1);
+	return (0);
+}
+
+int if_builtins_in_chiled(t_all *my_struct, int c_of_s)
+{
+	if(my_struct->each_cmd[c_of_s].cmd[0] && ft_strlen(my_struct->each_cmd[c_of_s].cmd[0]) \
+	 &&!ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "cd", ft_strlen(my_struct->each_cmd[c_of_s].cmd[0])))
+		return (1);
+	if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "export", ft_strlen("export")+1))
+		return (1);
+	if (my_struct->each_cmd[0].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "echo", ft_strlen("echo")+1))
+		return (1);
+	if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "pwd", ft_strlen("pwd")+1))
 		return (1);
 	else if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "env", ft_strlen("env")+1))
 		return (1);
@@ -191,8 +212,8 @@ int builtins(t_all *my_struct, int c_of_s)
 	 &&!ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "cd", ft_strlen(my_struct->each_cmd[c_of_s].cmd[0])))
 	{
 		if(cd_commade(my_struct, c_of_s))
-			return (-1);
-		return (1);
+			exit(1);
+		exit(0);
 	}
 	if (my_struct->each_cmd[0].cmd[0] && \
 	!ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "echo", ft_strlen("echo")+1))
@@ -212,18 +233,19 @@ int builtins(t_all *my_struct, int c_of_s)
 		export_command(my_struct,c_of_s);
 
 		if(my_struct->check == 1)
-			return (-1);
-		return (1);
+			exit(1);
+		exit(0);
+
 	}
 	else if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "env", ft_strlen("env")+1))
 	{
 		env_command(my_struct->list);
-		return (1);
+		exit(0);
 	}
 	else if (my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "unset", ft_strlen("unset")+1))
 	{
 		unset_command(my_struct,c_of_s);
-		return (1);
+		exit(0);
 	}
 	else if (my_struct->each_cmd[c_of_s].cmd && my_struct->each_cmd[c_of_s].cmd[0] && !ft_strncmp(my_struct->each_cmd[c_of_s].cmd[0], "exit", ft_strlen("exit")))
 	{
